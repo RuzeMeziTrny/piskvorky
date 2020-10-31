1a75e014a28c37857763233cf0d3a4499a92e1d9
Ivana Ruzickova <shopping@mezitrny.cz>
2020-10-31 22:10:58 +0100

    winning move and winning message

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const getPosition = (field) => {
     if (field === gameFields[i]) {
       return {
         row: Math.floor(i / boardSize),
-        column: fieldIndex % boardSize,
+        column: i % boardSize,
       };
     }
   }
@@ -38,14 +38,83 @@ const getField = (row, column) => {
 };
 
 const getSymbol = (field) => {
-  if (field.className === `game-field--${whoPlays}`) {
-    return whoPlays;
+  if (field.classList.contains(`game-field--circle`)) {
+    return 'circle';
+  } else if (field.classList.contains(`game-field--cross`)) {
+    return 'cross';
   }
 };
+
+const symbolsToWin = 5;
+
+const isWinningMove = (field) => {
+  const origin = getPosition(field);
+  const symbol = getSymbol(field);
+
+  let i;
+
+  let inRow = 1; // Jednička pro právě vybrané políčko
+  // Koukni doleva
+  i = origin.column;
+  while (i > 0 && symbol === getSymbol(getField(origin.row, i - 1))) {
+    inRow++;
+    i--;
+  }
+
+  // Koukni doprava
+  i = origin.column;
+  while (
+    i < boardSize - 1 &&
+    symbol === getSymbol(getField(origin.row, i + 1))
+  ) {
+    inRow++;
+    i++;
+  }
+
+  if (inRow >= symbolsToWin) {
+    return true;
+  }
+
+  let inColumn = 1;
+  // Koukni nahoru
+  i = origin.row;
+  while (i > 0 && symbol === getSymbol(getField(i - 1, origin.column))) {
+    inColumn++;
+    i--;
+  }
+
+  // Koukni dolu
+  i = origin.row;
+  while (
+    i < boardSize - 1 &&
+    symbol === getSymbol(getField(i + 1, origin.column))
+  ) {
+    inColumn++;
+    i++;
+  }
+
+  if (inColumn >= symbolsToWin) {
+    return true;
+  }
+
+  return false;
+};
+
+/* const i18n = {
+  circle: 'kolečko',
+  cross: 'křížek',
+}; */
 
 for (let i = 0; i < gameFields.length; i++) {
   gameFields[i].addEventListener('click', (event) => {
     addSymbol(event);
-    changePlayer();
+    if (isWinningMove(event.target)) {
+      const winningMessage = () => {
+        alert(`${whoPlays} wins, congratulations :)`);
+      };
+      setTimeout(winningMessage, 100);
+    } else {
+      changePlayer();
+    }
   });
 }
